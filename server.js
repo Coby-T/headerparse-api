41124497d46f431f875b566f0f04d43545aab55e
Coby Tao <cobytao@hotmail.com>
2016-07-27 21:46:02 +0000

    working build

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express")
+var app = express()
+
+const port = 8080
+
+app.get('/whoami/', function(req, res) {
+    const osregex = /(\([A-Za-z0-9;,./\s]*\))/g
+    const ipregex = /(\d+.)+/g
+    var ip = ipregex.exec(req.ip)[0]
+    var os = osregex.exec(req.headers['user-agent'])[0].slice(1,-1)
+    var lang = req.headers['accept-language'].split(";")[0]
+    res.json({
+        IPAddress: ip,
+        OperatingSystem: os,
+        Language: lang
+    })
+})
+
+app.listen(process.env.PORT || port)
